a094351d3ff0e3dc274f3b79883575f9db04eb4a
Oles <olesbychevskyi@gmail.com>
2021-12-10 18:14:23 +0200

    methods, const, constr are added

---
+++ src/com/company/MyKeyAdapter.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class MyKeyAdapter extends KeyAdapter {
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+}
--- src/com/company/SnakeGame.java
+++ src/com/company/SnakeGame.java
@@ -1,8 +1,8 @@
 package com.company;
 
-public class Main {
+public class SnakeGame {
 
     public static void main(String[] args) {
-	// write your code here
+	GameFrame frame = new GameFrame();
     }
 }
